086350d76d7653c94cb997c931a659e40c76177e
dominicddl <e1398293@u.nus.edu>
2025-08-22 16:02:35 +0800

    Renamed Duke class to Luffy and changed the  greeting message

---
+++ src/main/java/Luffy.java
@@ -0,0 +1,12 @@
+public class Luffy {
+    public static void main(String[] args) {
+        String greet = "____________________________________________________________\n"
+                + "Hello! I'm Luffy\n"
+                + "What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+       
+        System.out.println(greet);
+    }
+}
