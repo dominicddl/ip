2087db4d6460f34b6c7dbdd0e1f21b252a279e44
dominicddl <e1398293@u.nus.edu>
2025-08-23 00:18:41 +0800

    Created LuffyException for error handling

---
+++ src/main/java/LuffyException.java
@@ -0,0 +1,9 @@
+public class LuffyException extends Exception {
+    public LuffyException(String message) {
+        super(message);
+    }
+
+    public String getMessage() {
+        return "OOPS!!! " + super.getMessage();
+    }
+}
