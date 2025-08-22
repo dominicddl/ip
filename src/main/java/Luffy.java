3e535d43cc8f85cb8841577f6665fdfc3f210142
dominicddl <e1398293@u.nus.edu>
2025-08-22 17:39:51 +0800

    Abstracted Luffy class to use Task objects

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -8,7 +8,7 @@ public class Luffy {
     String goodbye = "____________________________________________________________\n"
             + "Bye! See you next time!\n" + "I'll be waiting for you to join my crew!\n"
             + "____________________________________________________________\n";
-    ArrayList<String> tasks = new ArrayList<>();
+    ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Luffy luffy = new Luffy();
@@ -18,16 +18,18 @@ public class Luffy {
         // While scanner is open, keep asking for input
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
+            Task task = new Task(input);
             if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {
                 System.out.println(luffy.goodbye);
                 break;
             } else if (input.equals("list") || input.equals("List") || input.equals("LIST")) {
                 for (int i = 0; i < luffy.tasks.size(); i++) {
-                    System.out.println(i + 1 + ". " + luffy.tasks.get(i));
+                    System.out.println(i + 1 + ". " + luffy.tasks.get(i).getStatusIcon() + " "
+                            + luffy.tasks.get(i).getDescription());
                 }
             } else {
-                luffy.tasks.add(input);
-                System.out.println("Task added: " + input);
+                luffy.tasks.add(task);
+                System.out.println("Task added: " + task.getDescription());
             }
         }
         sc.close();
