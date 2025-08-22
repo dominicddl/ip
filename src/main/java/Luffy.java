569c76c82f04c5919ca8e7049fbf76045c85a806
dominicddl <e1398293@u.nus.edu>
2025-08-22 17:19:51 +0800

    Update greeting message and add task listing feature in Luffy class

--- src/main/java/Luffy.java
+++ src/main/java/Luffy.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Luffy {
     String greet = "____________________________________________________________\n"
-            + "Hello! I'm Luffy\n" + "Do you want to be my crewmate?\n"
+            + "Hello! I'm Luffy\n" + "Be my crewmate!\n"
             + "____________________________________________________________\n";
     String goodbye = "____________________________________________________________\n"
             + "Bye! See you next time!\n" + "I'll be waiting for you to join my crew!\n"
@@ -21,6 +21,10 @@ public class Luffy {
             if (input.equals("bye") || input.equals("Bye") || input.equals("BYE")) {
                 System.out.println(luffy.goodbye);
                 break;
+            } else if (input.equals("list") || input.equals("List") || input.equals("LIST")) {
+                for (int i = 0; i < luffy.tasks.size(); i++) {
+                    System.out.println(i + 1 + ". " + luffy.tasks.get(i));
+                }
             } else {
                 luffy.tasks.add(input);
                 System.out.println("Task added: " + input);
