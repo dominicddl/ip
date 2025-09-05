ca5965ff3292b45b05b158e537d41b4b2b2c0c2e
dominicddl <e1398293@u.nus.edu>
2025-09-05 16:31:28 +0800

    Use FXML for UI layour

--- src/main/java/luffy/DialogBox.java
+++ src/main/java/luffy/DialogBox.java
@@ -1,7 +1,12 @@
 package luffy;
 
+import java.io.IOException;
+import java.util.Collections;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -10,29 +15,28 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 /**
- * Custom control representing a dialog box consisting of an ImageView to represent the speaker's
- * face and a label containing text from the speaker.
+ * Represents a dialog box consisting of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
  */
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayPicture;
 
-    /**
-     * Creates a dialog box with the specified text and image.
-     *
-     * @param text the text to display in the dialog box
-     * @param img the image to display as the speaker's avatar
-     */
     private DialogBox(String text, Image img) {
-        this.text = new Label(text);
-        displayPicture = new ImageView(img);
+        try {
+            FXMLLoader fxmlLoader =
+                    new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        this.text.setWrapText(true);
-        displayPicture.setFitWidth(100.0);
-        displayPicture.setFitHeight(100.0);
-
-        this.setAlignment(Pos.TOP_RIGHT);
-        this.getChildren().addAll(this.text, displayPicture);
+        dialog.setText(text);
+        displayPicture.setImage(img);
     }
 
     /**
@@ -40,29 +44,15 @@ public class DialogBox extends HBox {
      */
     private void flip() {
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
+        Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
     }
 
-    /**
-     * Creates a dialog box for user input.
-     *
-     * @param text the user's input text
-     * @param img the user's avatar image
-     * @return a DialogBox representing the user's message
-     */
     public static DialogBox getUserDialog(String text, Image img) {
         return new DialogBox(text, img);
     }
 
-    /**
-     * Creates a dialog box for Luffy's response.
-     *
-     * @param text Luffy's response text
-     * @param img Luffy's avatar image
-     * @return a DialogBox representing Luffy's message, with flipped layout
-     */
     public static DialogBox getLuffyDialog(String text, Image img) {
         var db = new DialogBox(text, img);
         db.flip();
